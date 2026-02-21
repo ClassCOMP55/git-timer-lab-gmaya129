5553ad653eef2ff1bd5f1b0082813856b4bdfa01
GorgeMaya <gmaya209@gmail.com>
2026-02-21 11:58:07 -0800

    balls moved across the screen

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -9,6 +9,10 @@ public class BallLauncher extends GraphicsProgram{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
+	public static final int MS = 50;	// timer fires every 50ms
+	public static final int SPEED = 2;	// pixels to move per tick
+	
+	private ArrayList<GOval> balls;	// tracks all balls on screen
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,12 +20,24 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		balls = new ArrayList<GOval>();	// init the list
+		
+		Timer movement = new Timer(MS, this);	// create and start timer
+		movement.start();
+		
 		addMouseListeners();
 	}
 	
 	public void mousePressed(MouseEvent e) {
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
+		balls.add(ball);	// add to tracking list
+	}
+	
+	public void actionPerformed(ActionEvent e) {	// moves all balls every tick
+		for (GOval ball : balls) {
+			ball.move(SPEED, 0);
+		}
 	}
 	
 	public GOval makeBall(double x, double y) {
